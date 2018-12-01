9d0f0c29eb0f00d66fb795bb8a52143893cab050
Lauri Haara <lauri.haara@gmail.com>
2018-12-01 14:04:51 +0200

    5_17 & 5_18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import counterReducer from './reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Statistiikka = (props) => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  if (isNaN(props.prosentti)) {
+    return (
+      <div>
+        <h2>statistiikka</h2>
+        <div>ei yhtään palautetta annettu</div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+        <h2>statistiikka</h2> 
+        <div>  
+          <table>
+            <tbody>
+              <tr>
+                <td>hyvä</td>
+                <td>{store.getState().good}</td>
+              </tr>
+              <tr>          
+                <td>neutraali</td>
+                <td>{store.getState().ok}</td>
+              </tr>
+              <tr>
+                <td>huono</td>
+                <td>{store.getState().bad}</td>
+              </tr>
+              <tr>          
+                <td>positiivisia</td>
+                <td>{props.prosentti}%</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      <button onClick={e =>store.dispatch({ type: 'ZERO'})}>nollaa tilasto</button>
+    </div >
+  )
+}
+
+const store = createStore(counterReducer)
+
+
+class App extends React.Component {
+  klik = (nappi) => () => {
+    store.dispatch({ type: nappi })
+  }  
+
+  render() {
+    const hyvapros = (store.getState().good / (store.getState().good + store.getState().ok + store.getState().bad) * 100).toFixed(1)
+
+    return (
+      <div>
+        <h2>anna palautetta</h2>
+        <button onClick={this.klik('GOOD')}>hyvä</button>
+        <button onClick={this.klik('OK')}>neutraali</button>
+        <button onClick={this.klik('BAD')}>huono</button>
+        <Statistiikka prosentti = {hyvapros}/>
+      </div>
+    )
+  }
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
